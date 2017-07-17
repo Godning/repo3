92d2210ce5309de1fc88acc0c4e46bd3b3660bd4
Godning <736397792@qq.com>
2017-07-17 23:11:04 +0800

    update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
-module.exports = function main() {
+module.exports = function main(s) {
   // Write your cade here
+  var ans = 0;
+  for(var i=0;i<s.length;i++){
+    var tmp = s.charCodeAt(i) - 48;
+    if(tmp){
+      ans += tmp;
+    } 
+  }
+  return ans;
 };
